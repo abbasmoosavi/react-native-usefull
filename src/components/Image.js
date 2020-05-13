a7e16207b4511a31d90b548566d43786ea01ee26
arvin_abbas_ <moosaviabbas93@gmail.com>
2020-05-13 12:32:20 +0430

    add another props for Image

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,81 +1,106 @@
-import React, { Component, } from 'react'
-import { ImageStyle, ImageResizeMode, ImageSourcePropType } from 'react-native'
+import React, { Component } from 'react';
+import {
+  ImageStyle,
+  ImageResizeMode,
+  ImageSourcePropType,
+  ImageProgressEventDataIOS,
+  ImageLoadEventDataAndroid,
+} from 'react-native';
 import UFRN from '..';
 import FastImage from 'react-native-fast-image';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 type PropsType = {
+  /**
+   * @property
+   * image source,
+   * if you have url enter value exp: { uri: Your url } or
+   * if you have local image use require('path')
+   */
+  source: ImageSourcePropType,
+  /**
+   * @property
+   * image style, you can enter styles array
+   */
+  style?: ImageStyle | ImageStyle[],
+  /**
+   * @property
+   * image tint color,
+   * for change color base of image use this prop
+   */
+  tintColor: String,
+  /**
+   * @property
+   * image size,
+   * use on of 'cover' | 'contain' | 'stretch' | 'repeat' | 'center',
+   * and default is 'cover
+   */
+  resizeMode: ImageResizeMode,
 
-    /**
-     * @property
-     * image source,
-     * if you have url enter value exp: { uri: Your url } or 
-     * if you have local image use require('path')
-     */
-    source: ImageSourcePropType,
-    /**
-     * @property
-     * image style, you can enter styles array
-     */
-    style?: ImageStyle | ImageStyle[],
-    /**
-     * @property
-     * image tint color,
-     * for change color base of image use this prop
-     */
-    tintColor: String,
-    /**
-     * @property
-     * image size,
-     * use on of 'cover' | 'contain' | 'stretch' | 'repeat' | 'center',
-     * and default is 'cover
-     */
-    resizeMode: ImageResizeMode,
+  onProgress: ImageProgressEventDataIOS,
 
+  onLoadStart: ImageLoadEventDataAndroid,
+  onLoadEnd: ImageLoadEventDataAndroid,
 };
-type StateType = {
-};
+type StateType = {};
 
 class Image extends Component<PropsType, StateType> {
-    constructor(props) {
-        super(props)
-        this.state = {
-            height: UFRN.Values.IMAGE_HEIGHT,
-            width: UFRN.Values.IMAGE_WIDTH,
-        };
-    }
-    static defaultProps = {
-        resizeMode: 'cover',
-    }
-    static propsType = {
-        style: PropTypes.arrayOf(PropTypes.ImageStyle)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      height: UFRN.Values.IMAGE_HEIGHT,
+      width: UFRN.Values.IMAGE_WIDTH,
+    };
+  }
+  static defaultProps = {
+    resizeMode: 'cover',
+  };
+  static propsType = {
+    style: PropTypes.arrayOf(PropTypes.ImageStyle),
+  };
 
-    getResizeMode(value) {
-        switch (value) {
-            case 'cover':
-                return FastImage.resizeMode.cover
-            case 'contain':
-                return FastImage.resizeMode.contain
-            case 'center':
-                return FastImage.resizeMode.center
-            case 'stretch':
-                return FastImage.resizeMode.stretch
-        }
+  getResizeMode(value) {
+    switch (value) {
+      case 'cover':
+        return FastImage.resizeMode.cover;
+      case 'contain':
+        return FastImage.resizeMode.contain;
+      case 'center':
+        return FastImage.resizeMode.center;
+      case 'stretch':
+        return FastImage.resizeMode.stretch;
     }
-    render() {
-        const { height, width } = this.state
-        const { children, style, source, resizeMode, tintColor } = this.props
-        return (
-            <FastImage resizeMode={this.getResizeMode(resizeMode)} tintColor={tintColor !== undefined ? tintColor : null} style={[{ height, width }, style]} source={source}>
-                {children}
-            </FastImage>
-        )
-    }
+  }
+  render() {
+    const { height, width } = this.state;
+    const {
+      children,
+      style,
+      source,
+      resizeMode,
+      tintColor,
+      onProgress,
+      onLoadStart,
+      onLoadEnd,
+    } = this.props;
+    return (
+      <FastImage
+        onProgress={onProgress}
+        onLoadStart={onLoadStart}
+        onLoadEnd={onLoadEnd}
+        resizeMode={this.getResizeMode(resizeMode)}
+        tintColor={tintColor !== undefined ? tintColor : null}
+        style={[{ height, width }, style]}
+        source={source}
+      >
+        {children}
+      </FastImage>
+    );
+  }
 }
 
 Image.propsType = {
-    style: PropTypes.arrayOf(PropTypes.ImageStyle)
-}
+  style: PropTypes.arrayOf(PropTypes.ImageStyle),
+};
 
-export default Image
+export default Image;
